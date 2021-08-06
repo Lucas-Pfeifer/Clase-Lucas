2892bd79a89bf96959adb797d96fe86a3e321d52
jasanchez1 <jasanchez1@uc.cl>
2021-08-06 17:54:14 -0400

    update insert.py

--- insert.py
+++ insert.py
@@ -1,4 +1,4 @@
-lista = [0, 1, 2, 3, 4, 5, 7, 8, 9, 10]
+lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 #Funcion recibe 2 argumentos, el elemento a agregar y su posicion
 #Tip: len(lista) entrega el tamaño de la lista que estas trabajando
@@ -10,13 +10,31 @@ lista = [0, 1, 2, 3, 4, 5, 7, 8, 9, 10]
 #Ojo a los casos limites, ej estas agregando en la ultima posicion y no hay nada que desplazar o estas agregando en una posicion que no existe
 
 def insert(data, position, array):
-    A = array[0:position]
-    print(A)
+    if position == len(array) -1:
+        array.append(data)
+        return array
+
+    elif position == 0:
+        A = [data]
+        B = A + array
+        return B
+    
+    elif position >= len(array):
+        print("Index out of range!")
+        return array
+        
+    else:
+        A = array[0:position]
+        B = array[position:]
+        A.append(data)
+        C = A + B
+        return C
     
 
 
 
+new_list = insert(11, 99, lista)
 
-insert(1, 6, lista)
+print(new_list)
 
 
