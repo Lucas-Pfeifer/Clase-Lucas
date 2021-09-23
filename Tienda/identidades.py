8067510cc1c4eddd8c70248aa14899c5a1c405ec
Lucas Pfeifer <lucaspfeifer2008@gmail.com>
2021-09-23 14:45:08 -0400

    Test

--- Tienda/identidades.py
+++ Tienda/identidades.py
@@ -1,41 +1,40 @@
 class Quiosco:
 
-
-
     def __init__(self):
         self.productos = {}
-    
+
     def registrar_venta(self, Producto):
         pass
-        
 
     def recibir_pedido(self, path):
         recibidos = open(path, "r", encoding='UTF-8').readlines()
         primera_linea = True
-        for line in recibidos: 
+        for line in recibidos:
             if primera_linea:
                 primera_linea = False
             else:
                 datos = line.strip().split(";")
-                Nuevo_Producto = Producto(datos[1], datos[2], datos[0], datos[3], self)
+                Nuevo_Producto = Producto(
+                    datos[1], datos[2], datos[0], datos[3], self)
                 self.productos[Nuevo_Producto.id] = Nuevo_Producto
-   
-    def mostrar_stock(self, carro): 
+
+    def mostrar_stock(self, carro):
         for key in self.productos:
             aux = 0
             if key in carro:
                 aux = carro[key]
-            print(f" Nombre: {self.productos[key].nombre}\n ID: {self.productos[key].id} \n Precio: {self.productos[key].precio}\n Cantidad: {self.productos[key].stock - aux}")
+            print(
+                f" Nombre: {self.productos[key].nombre}\n ID: {self.productos[key].id} \n Precio: {self.productos[key].precio}\n Cantidad: {self.productos[key].stock - aux}")
             print(f"----------------------------------------------------------------------------------------------")
-            
+
     def validar(self, id):
         if id in self.productos:
-            return True  
-        return False          
-
+            return True
+        return False
 
     def cambiar_stock(self, productoId, cantidad):
         pass
+
 
 class Producto:
 
@@ -48,18 +47,11 @@ class Producto:
         self.stock = int(stock)
 
     def validar(self, cantidad):
-         
+
         if cantidad > self.stock:
             return False
-            
-        return True
 
+        return True
 
     def compra(self, cantidad):
         self.stock -= cantidad
-    
-
-
-
-
-    
